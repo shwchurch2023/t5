999b0164707403f07fb9db0d4b41668355dc6eb5
Shwchurch <shwchurch3@gmail.com>
2022-01-29 13:10:07 -0800

    Fix cron script bug

--- bin/cron-sync.sh
+++ bin/cron-sync.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 
-if [[ -z "$BASE_PATH" ]];then
-    cd "$(dirname "$0")"
-    cd ..
-    export BASE_PATH=$(pwd)
-    source $BASE_PATH/bin/common.sh
+# if [[ -z "$BASE_PATH" ]];then
+#     cd "$(dirname "$0")"
+#     cd ..
+#     export BASE_PATH=$(pwd)
+#     source $BASE_PATH/bin/common.sh
+# fi
+
+if [[ `whoami` != 'root' ]]; then
+    echo "[ERROR] The cron generator should be executed with root/sudo"
+    exit 2
 fi
+
+cd "$(dirname "$0")"
+cd ..
+
+BASE_PATH=$(pwd)
 
 cd $BASE_PATH
 
@@ -19,10 +29,10 @@ logPath=${hugoGithubRoot}/sync.log
 scriptToRun="/bin/bash ${syncFile} > ${logPath} 2>&1"
 
 currentUser=$(whoami)
-hugoUserGroup=hugo
-sudo useradd -U $hugoUserGroup
-sudo chown -R $hugoUserGroup.$hugoUserGroup .
-sudo usermod -a -G $hugoUserGroup $currentUser
+
+# sudo useradd -U $hugoUserGroup
+sudo chown -R hugo.hugo .
+# sudo usermod -a -G $hugoUserGroup $currentUser
 sudo chmod -R g+rw $hugoGithubRoot
 ls -la $hugoGithubRoot
 #echo "1 14 * * * $hugoUserGroup ${scriptToRun}" > ${cronScriptPath}
