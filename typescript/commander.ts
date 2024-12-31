6f5a5a6dd6b96804c29915b3be98133382212b1d
shwchurch3 <lanshunfang@gmail.com>
2024-12-30 21:19:10 -0600

    Stop printing out file path processed

--- typescript/commander.ts
+++ typescript/commander.ts
@@ -24,7 +24,7 @@ async function replaceUrlCommander() {
             await walk({
                 dir: p,
                 callback: async(filePath) => {
-                    console.log(filePath);
+                    // console.log(filePath);
                     let content = readFileSync(filePath).toString();
 
                     const pattList = [
