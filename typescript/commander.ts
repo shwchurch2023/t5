8a42af344e9d3791fc2a8119686ec14e97709ada
shwchurch3 <lanshunfang@gmail.com>
2025-01-04 16:45:03 -0600

    Enable more capabilities for wpHugoExporterAfterAllProcessor

--- typescript/commander.ts
+++ typescript/commander.ts
@@ -7,48 +7,78 @@ const program = new Command();
 async function replaceUrlCommander() {
 
     const cmd = `wpHugoExporterAfterAllProcessor`;
+
+    const keyValueSplitter = `____`;
     program
         .command(cmd)
         .usage(`npx ts-node typescript/commander.ts wpHugoExporterAfterAllProcessor --path $(pwd)/content`)
+        .usage(`npx ts-node typescript/commander.ts wpHugoExporterAfterAllProcessor --path $(pwd)/shwchurch7.github.io --file_ext=*.html --replace_def=https://shwchurch7.github.io/wp-content/uploads/2008${keyValueSplitter}https://shwchurch4.github.io/wp-content/uploads/2008`)
         .addOption(
             new Option(
-                '--path <path>', 
+                '--path <path>',
                 'The path of the markdown files'
             )
-            .makeOptionMandatory()
+                .makeOptionMandatory()
+        ) 
+        .addOption(
+            new Option(
+                '--file_ext <file_ext...>',
+                'The file extension to filter'
+            )
+                .default(
+                    [`.md`]
+                )
+        )
+        .addOption(
+            new Option(
+                '--replace_def <replace_def...>',
+                `e.g --replace_def=https://shwchurch7.github.io/wp-content/uploads/2008${keyValueSplitter}https://shwchurch4.github.io/wp-content/uploads/2008`
+            )
         )
         .action(async (str, options) => {
             const p = options.getOptionValue('path');
+            const file_ext = options.getOptionValue('file_ext');
+            const replace_def = options.getOptionValue('replace_def');
             // Example usage:
 
             await walk({
                 dir: p,
-                callback: async(filePath) => {
+                callback: async (filePath) => {
                     // console.log(filePath);
                     let content = readFileSync(filePath).toString();
 
-                    const pattList = [
-                        {
-                            pattern: /http(s)?:\/\/.+?\.shwchurch.org\//gi,
-                            replacement: `/`
-                        },
-                        {
-                            pattern: /<\/wp-content.+?>/gi,
-                            replacement: ``
-                        },
-                        {
-                            pattern: /date: -00[0-9-]+T00:00:00\+00:00/,
-                            replacement: `date: 2020-12-28T03:17:00+00:00`,
-                            replaceFunc: (content: string) => {
-                                return '';
+                    const pattList = replace_def
+                        ? replace_def.map(def => {
+
+                            const split = def.split(keyValueSplitter)
+
+                            return {
+                                pattern: new RegExp(split[0]),
+                                replacement: split[1],
+                            };
+                        })
+                        : [
+                            {
+                                pattern: /http(s)?:\/\/.+?\.shwchurch.org\//gi,
+                                replacement: `/`
                             },
-                        },
-                    ];
+                            {
+                                pattern: /<\/wp-content.+?>/gi,
+                                replacement: ``
+                            },
+                            {
+                                pattern: /date: -00[0-9-]+T00:00:00\+00:00/,
+                                replacement: `date: 2020-12-28T03:17:00+00:00`,
+                                replaceFunc: (content: string) => {
+                                    return '';
+                                },
+                            },
+                        ];
 
                     pattList.forEach(
                         ({ pattern, replacement, replaceFunc }) => {
                             // console.log(`Replace pattern ${pattern} with / in ${filePath}`);
-                            content = replaceFunc ? (pattern.test(content) ? replaceFunc(content) : content ) : content.replace(pattern, replacement);
+                            content = replaceFunc ? (pattern.test(content) ? replaceFunc(content) : content) : content.replace(pattern, replacement);
                         }
                     );
 
@@ -65,7 +95,7 @@ async function replaceUrlCommander() {
 
                 },
                 filter: (filePath) => {
-                    return /\.md$/.test(filePath)
+                    return file_ext.some(ext => filePath.endsWith(ext))
                 }
             });
 
